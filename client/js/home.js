f6fd6efd8ee8e9b7be00f96f9135a3eef90ff96d
Muhammed Patel <muhammedp903@gmail.com>
2023-03-10 11:47:48 +0000

    Fix home page UI including date-picker.

--- client/js/home.js
+++ client/js/home.js
@@ -1,8 +1,16 @@
 
+function getTasks() {
+    // GET request to retrieve tasks from the database for the current user
+}
+
+getTasks(); // TODO: Store the returned tasks to show later
+
 window.addEventListener('load', () =>{
     const form = document.querySelector("#new-task-form");
     const input = document.querySelector("#new-task-input");
     const list_el = document.querySelector("#tasks");
+
+    // TODO: Display the tasks retrieved from the database
 
     form.addEventListener('submit' , (e) => {
         e.preventDefault();
@@ -77,7 +85,6 @@ window.addEventListener('load', () =>{
 
     });
 
+
     
-        
-    }
-);
+});
